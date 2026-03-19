4d7067d42c670e39ea15ed15bef8cca6187001e2
iamCoderMohit <mohitjoshiu@gmail.com>
2026-03-19 15:49:48 +0530

    fix cors error 0.2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,13 @@ import canvas from "./routes/canvas.route"
 const app = new Hono()
 
 app.use(
-    "/api/v1/*",
-    cors({
-        origin: ["http://localhost:3000", "https://code-us-orcin.vercel.app"],
-        credentials: true
-    })
+  "*",
+  cors({
+    origin: "https://code-us-orcin.vercel.app",
+    credentials: true,
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
 )
 
 app.route("/api/v1/auth", auth)
